1eabcf446334177d89d930a872f4497d7ec85934
Dave98-dev <71784042+Dave98-dev@users.noreply.github.com>
2024-04-20 17:50:30 +0200

    aggiunti ciclo per script powershell

--- AssemblerVideo/process.ps1
+++ AssemblerVideo/process.ps1
@@ -1,35 +1,47 @@
-$random_number = Get-Random -Minimum 500 -Maximum 20000
-$video_length = 60
+try {
+    $random_number = Get-Random -Minimum 60 -Maximum 3000
+    $video_length = 60
+    echo "chiamo il sito"
+    $value = Invoke-RestMethod -Uri "http://127.0.0.1:8000/reddit/get-single-reddit"
 
+    $posts = @($value)
+    $i = 0
 
+    foreach ($post in $posts){
+        $i = $i + 1
+        echo "genero l'audio"
+        .\consoleApp\ConsoleApp1.exe $post
+        cp .\consoleApp\test.wav .\input\audio.wav
 
-echo "chiamo il sito e genero un audio"
-.\consoleApp\ConsoleApp1.exe
-cp .\consoleApp\test.wav .\input\audio.wav
+        echo "genero un video di $video_length secondi partendo dal secondo $random_number"
 
+        ffmpeg -i input\video.mp4 -ss $random_number -t $video_length output\subway_clip.mp4 -y
+        ffmpeg -i input\audio.wav -t $video_length -acodec pcm_s16le -ac 1 -ar 16000 output\audio.wav -y
 
-echo "genero un video di $video_length secondi partendo dal secondo $random_number"
+        ffmpeg -i output\subway_clip.mp4 -i output\audio.wav -c:v copy -map 0:v:0 -map 1:a:0 output\video_finale.mp4 -y
 
-ffmpeg -i input\video.mp4 -ss $random_number -t $video_length output\subway_clip.mp4 -y
-ffmpeg -i input\audio.wav -t $video_length -acodec pcm_s16le -ac 1 -ar 16000 output\audio.wav -y
+        cp .\output\audio.wav .\my-video\temp\audio.wav
 
-ffmpeg -i output\subway_clip.mp4 -i output\audio.wav -c:v copy -map 0:v:0 -map 1:a:0 output\video_finale.mp4 -y
+        cd .\my-video\whisper.cpp
 
-cp .\output\audio.wav .\my-video\temp\audio.wav
+        .\main.exe -f ..\temp\audio.wav -oj ..\temp\output.json -m .\ggml-medium.en.bin
 
-cd .\my-video\whisper.cpp
+        cd ..
 
-.\main.exe -f ..\temp\audio.wav -oj ..\temp\output.json -m .\ggml-medium.en.bin
+        cp .\temp\audio.wav.json .\public\sample-video.json
 
-cd ..
+        cp ..\output\video_finale.mp4 .\public\sample-video.mp4
 
-cp .\temp\audio.wav.json .\public\sample-video.json
+        npm run build
 
-cp ..\output\video_finale.mp4 .\public\sample-video.mp4
+        cp .\out\CaptionedVideo.mp4 ..\output\videos\CaptionedVideo_$i.mp4
 
-npm run build
+        cd ..
+    }
+}
+catch {
 
-cp .\out\CaptionedVideo.mp4 ..\output\CaptionedVideo.mp4
+    Write-Host $_.Exception.Message
+        exit
 
-cd ..
-
+}
